ae8b6870ca723c971912b2d42c20bff3006374a7
Juan-Lengyel <juan.lengyel@globant.com>
2020-04-24 09:11:23 -0500

    Functional flash cards web app with no db implementation

---
+++ flashcards.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, abort
+
+from model import db
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+  return render_template(
+    'welcome.html',
+    cards=db
+  )
+
+@app.route('/card/<int:index>')
+def card_view(index):
+  try:
+    card = db[index]
+    print(card)
+    return render_template(
+      'card_view.html',
+      card=card,
+      index=index,
+      max_index=len(db)-1
+    )
+  except IndexError:
+    abort(404)
+
